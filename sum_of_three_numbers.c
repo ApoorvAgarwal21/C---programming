c15ff8a3a22674969f0e43d0fe548eb5c48f4e67
ApoorvAgarwal21 <32730652+ApoorvAgarwal21@users.noreply.github.com>
2018-02-09 22:17:56 +0530

    sum_of_three_numbers.c

---
+++ sum_of_three_numbers.c
@@ -0,0 +1,13 @@
+// sum of 3 integers or real values//
+#include<stdio.h>
+int main() {
+float x, y, z, r;
+	printf("enter the value of x\n");
+	scanf("%f", &x);
+	printf("enter the value of y\n");
+	scanf("%f", &y);
+	printf("enter the value of z\n");
+	scanf("%f", &z);
+r = x + y + z;
+	printf("sum of three numbers is %f", r);
+}
